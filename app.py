15d5aa7be0e9664a8a864a94bd07041b50c969a5
Medea <logmedha09@gmail.com>
2022-11-04 07:29:31 +0000

    added a colour scheme

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect
 from functions import addemail, verifyemail, unsubscribe
+from lists import contacts, projects
+
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
-names = ['Munity', 'OvalEyes', 'Drinks Cabin', 'FRANK', 'We Greek', 'Imposter', 'Jasonism', 'BesucheResort', 'What If']
-urls = ['https://munity.vulcanwm.repl.co/', 'https://ovaleyes.ovaleyes.repl.co/', 'https://drinks-cabin.vulcanwm.repl.co/', 'https://frank.vulcanwm.repl.co/', 'https://wegreek.vulcanwm.repl.co/', 'https://imposter.vulcanwm.repl.co/', 'https://jasonism.vulcanwm.repl.co/', 'https://BesucheResort.vulcanwm.repl.co/', 'https://what-if.vulcanwm.repl.co']
-descriptions = ['a music related game', 'a social media website', 'a game in which you have your own drinks company', 'a multi-purpose website', 'a greek mythology wikipedia', 'guess who is the imposter in your online lesson', 'make your block as mighty as the immortal block of wood god Jason', 'virtual resort where you search for money', 'a website where you will face hypothetical scenarios']
-numbers = [0,1,2,3,4,5,6,7,8]
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
+@app.route('/contact')
+def contact():
+  return render_template("contact.html", contacts=contacts)
+
 @app.route("/projects")
-def projects():
-  return render_template("projects.html", names=names, urls=urls, descriptions=descriptions, numbers=numbers)
+def projects_page():
+  # return render_template("projects.html", names=names, urls=urls, descriptions=descriptions, numbers=numbers)
+  return render_template("projects.html", projects=projects)
 
 @app.route("/newsletter")
 def newsletterpage():
---
+++ lists.py
@@ -0,0 +1,62 @@
+contacts = {"Github": {"Image": 'static/logos/github.png', "Link": "https://github.com/VulcanWM"}, "Mail": {"Image": "static/logos/mail.png", "Link": "mailto:vulcanwmemail@gmail.com"}, "Replit": {"Image": "static/logos/replit.png", "Link": "https://replit.com/@VulcanWM"}, "dev.to": {"Image": "static/logos/devto.png", "Link": "https://dev.to/vulcanwm"}}
+
+names = ['Distribute Time', 'Colourful Starry Night', 'What If', 'Munity', 'OvalEyes', 'Drinks Cabin', 'FRANK', 'We Greek', 'Imposter', 'Jasonism', 'BesucheResort']
+urls = ['https://distribute-time.vulcanwm.repl.co', 'https://colourful-starry-night.vulcanwm.repl.co', 'https://what-if.vulcanwm.repl.co', 'https://munity.vulcanwm.repl.co/', 'https://ovaleyes.ovaleyes.repl.co/', 'https://drinks-cabin.vulcanwm.repl.co/', 'https://frank.vulcanwm.repl.co/', 'https://wegreek.vulcanwm.repl.co/', 'https://imposter.vulcanwm.repl.co/', 'https://jasonism.vulcanwm.repl.co/', 'https://BesucheResort.vulcanwm.repl.co/']
+descriptions = ['a website where you can automatically make your own timetable', 'a colourful starry night generator', 'a website where you will face hypothetical scenarios', 'a music related game', 'a social media website', 'a game in which you have your own drinks company', 'a multi-purpose website', 'a greek mythology wikipedia', 'guess who is the imposter in your online lesson', 'make your block as mighty as the immortal block of wood god Jason', 'virtual resort where you search for money']
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
+
+
+projects = {
+  "Distribute Time": {
+    "Link": "https://distribute-time.vulcanwm.repl.co",
+    "Desc": "automatically make your own timetable"
+  }, 
+  "Escape the Punch": {
+    "Link": "https://escape-the-punch.vulcanwm.repl.co",
+    "Desc": "try to escape the ghost's punch"
+  },
+  "What If": {
+    "Link": "https://what-if.vulcanwm.repl.co",
+    "Desc": "a website where you will face hypothetical scenarios"
+  }, 
+  "Jason Clicker": {
+    "Link": "https://jasonism.vulcanwm.repl.co",
+    "Desc": "try to make jason stronger by clicking"
+  }, 
+  "Colourful Starry Night": {
+    "Link": "https://colourful-starry-night.vulcanwm.repl.co",
+    "Desc": "a colourful starry night generator"
+  }, 
+  "Munity": {
+    "Link": "https://munity.vulcanwm.repl.co",
+    "Desc": "test yourself on song lyrics"
+  }, 
+  "OvalEyes": {
+    "Link": "https://ovaleyes.ovaleyes.repl.co",
+    "Desc": "a social media website"
+  }, 
+  "We Greek": {
+    "Link": "https://wegreek.vulcanwm.repl.co",
+    "Desc": "a greek mythology wikipedia"
+  }, 
+  "Jasonism": {
+    "Link": "https://jasonism.vulcanwm.repl.co",
+    "Desc": "make your block of wood mighty"
+  }, 
+  "FRANK": {
+    "Link": "https://frank.vulcanwm.repl.co",
+    "Desc": "a multi-purpose website"
+  }, 
+  "Drinks Cabin": {
+    "Link": "https://drinks-cabin.vulcanwm.repl.co",
+    "Desc": "own your own drinks company"
+  }, 
+  "BesucheResort": {
+    "Link": "https://besucheresort.vulcanwm.repl.co",
+    "Desc": "search for money in a resort"
+  }, 
+  "Imposter": {
+    "Link": "https://imposter.vulcanwm.repl.co",
+    "Desc": "guess who is the imposter in your online lesson"
+  }
+}
